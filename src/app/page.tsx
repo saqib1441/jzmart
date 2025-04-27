88cf9ff721ed5b649b5d07be4dea116cf603f48b
saqi1441 <msaqibalilolai@gmail.com>
2025-04-27 16:17:03 +0500

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,10 @@
 "use client";
-
-import { Button } from "@/components/ui/button";
-import localforage from "@/utils/storage";
 import { FC } from "react";
-import toast from "react-hot-toast";
 
 const HomePage: FC = () => {
-  const storageHandler = () => {
-    localforage
-      .clear()
-      .then(() => {
-        window.location.reload();
-        return toast.success("Storage cleared successfully");
-      })
-      .catch((err) => {
-        // This code runs if there were any errors
-        console.error(err);
-      });
-  };
   return (
     <div className="flex flex-col h-screen justify-center items-center gap-5">
       <h1 className="font-bold text-3xl">Home Page</h1>
-      <Button onClick={storageHandler}>Clear Localforage Data</Button>
     </div>
   );
 };
